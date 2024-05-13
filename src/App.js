d490c0211fe0ca43f6ac8a9909cc472cdd258a21
shubham12-prof <shubhsemwal12@gmail.com>
2024-05-13 16:09:27 +0530

    refresh change6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter , Route, Router , Redirect } from "react-router-dom";
+import { BrowserRouter , Route, Switch , Redirect } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -9,21 +9,20 @@ import Services from './components/pages/Services';
 import ScrollToTop from './components/ScrollToTop';
 import Tours from './components/pages/Tours'
 import About from './components/pages/About'
-import { Router } from 'react-router-dom/cjs/react-router-dom.min';
 const App = () => {
   return (
     <>
-      <BrowserRouter> 
+      <BrowserRouter basename = "/reacttravel"> 
         <ScrollToTop />
         <Navbar /> 
-         <Router basename ="/reacttravel" >
+         <Switch>
           <Route exact path ="/" component ={Home}  />
           <Route exact path ="/About" component ={About} />
           <Route exact path ="/Tours" component ={Tours} />
           <Route exact path ="/Services" component ={Services} />
           <Route exact path ="/Contact" component ={Contact} />
           <Redirect to ='/' />
-      </Router> 
+      </Switch> 
 <Footer />
 </BrowserRouter> 
 </>
